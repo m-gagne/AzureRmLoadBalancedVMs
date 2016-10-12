032bee3ad628bb45801f9e53122a26aef3383a85
Marc Gagne <Marc Gagne>
2016-10-11 20:38:22 -0400

    Updated setup script

--- README.md
+++ README.md
@@ -11,5 +11,7 @@ To customize see `parameters` section of `template.json`
 
 ## setupVM.ps1
 
+> NOTE: You will need Windows Management Framework 5.0 on the target server. If running in Azure you should be fine. If you are testing on a local VM, [download it here](https://www.microsoft.com/en-us/download/details.aspx?id=50395)
+
 Powershell script to be run on the host VM to install [Chocolatey](https://chocolatey.org/), [git](https://chocolatey.org/packages/git), and execute the DSC (Desired State Config) `webServerConfig` to install IIS, .NET 4.5 & point to this [sample web app](https://github.com/m-gagne/ServerInfoDemoApp-DotNet).
  
--- setupVM.ps1
+++ setupVM.ps1
@@ -1,5 +1,24 @@
+# Install Chocolatey (package manager for Windows) 
 $env:chocolateyUseWindowsCompression = 'false'
 iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+
+# Install git
 choco install git -Confirm:$false
+
+# Install Chrome to test webapps easier
+choco install googlechrome -Confirm:$false
+
+# Update the Path which should now include git
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
-git clone https://github.com/m-gagne/ServerInfoDemoApp-DotNet.git c:\www
+
+# Clone the setup scripts
+git clone https://github.com/m-gagne/AzureRmLoadBalancedVMs.git c:\VmDsc
+
+# Run the DSC to install IIS, .NET etc.
+Start-DscConfiguration -Path .\VmDsc\WebServerConfig\ -Wait -Verbose 
+
+# Empty the default wwwroot site
+Remove-Item c:\inetpub\wwwroot\* -Recurse -Force -Confirm:$false
+
+# Clone the demo app into the
+git clone https://github.com/m-gagne/ServerInfoDemoApp-DotNet.git c:\inetpub\wwwroot

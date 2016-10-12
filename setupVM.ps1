77c012542aeee9142a0d93612f3b172ad526f74c
Marc Gagne <Marc Gagne>
2016-10-12 14:54:31 -0400

    Fixed path issue for DSC

--- setupVM.ps1
+++ setupVM.ps1
@@ -1,3 +1,6 @@
+# Variables
+$dscRootPath = "c:\VmDsc"
+
 # Install Chocolatey (package manager for Windows) 
 $env:chocolateyUseWindowsCompression = 'false'
 iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
@@ -12,10 +15,10 @@ choco install googlechrome -Confirm:$false
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
 
 # Clone the setup scripts
-git clone https://github.com/m-gagne/AzureRmLoadBalancedVMs.git c:\VmDsc
+git clone https://github.com/m-gagne/AzureRmLoadBalancedVMs.git $dscRootPath
 
 # Run the DSC to install IIS, .NET etc.
-Start-DscConfiguration -Path .\VmDsc\WebServerConfig\ -Wait -Verbose 
+Start-DscConfiguration -Path $dscRootPath\WebServerConfig -Wait -Verbose 
 
 # Empty the default wwwroot site
 Remove-Item c:\inetpub\wwwroot\* -Recurse -Force -Confirm:$false

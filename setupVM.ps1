694568de3e3087c21358e1cef9783ee37e344d66
Marc Gagne <Marc Gagne>
2016-10-12 15:18:40 -0400

    Moved logging from template to setup script

--- setupVM.ps1
+++ setupVM.ps1
@@ -1,8 +1,8 @@
 # Variables
-$dscRootPath = "c:\VmDsc"
-$setupScript = "https://github.com/m-gagne/AzureRmLoadBalancedVMs.git"
-$demoAppRepo = "https://github.com/m-gagne/ServerInfoDemoApp-DotNet.git"
-$wwwRoot = "c:\inetpub\wwwroot"
+$dscRootPath  = "c:\VmDsc"
+$setupScript  = "https://github.com/m-gagne/AzureRmLoadBalancedVMs.git"
+$demoAppRepo  = "https://github.com/m-gagne/ServerInfoDemoApp-DotNet.git"
+$wwwRoot      = "c:\inetpub\wwwroot"
 
 # Install Chocolatey (package manager for Windows) 
 $env:chocolateyUseWindowsCompression = 'false'
@@ -18,13 +18,13 @@ choco install googlechrome -Confirm:$false
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
 
 # Clone the setup scripts
-git clone $setupScript $dscRootPath
+git clone "$setupScript" "$dscRootPath"
 
 # Run the DSC to install IIS, .NET etc.
-Start-DscConfiguration -Path $dscRootPath\WebServerConfig -Wait -Verbose 
+Start-DscConfiguration -Path "$dscRootPath\WebServerConfig" -Wait -Verbose | Out-File "$dscRootPath\WebServerConfig.log"
 
 # Empty the default wwwroot site
-Remove-Item $wwwRoot\* -Recurse -Force -Confirm:$false
+Remove-Item "$wwwRoot\*" -Recurse -Force -Confirm:$false
 
 # Clone the demo app into the
-git clone $demoAppRepo $wwwRoot
+git clone "$demoAppRepo" "$wwwRoot"
